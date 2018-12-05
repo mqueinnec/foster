01eaa6f5282a96fa6d1fa78af60bdd1cb98e4aba
mqueinnec <martinqueinnec@gmail.com>
2018-12-05 12:29:12 -0800

    Update MatchExtent

--- R/MatchExtent.R
+++ R/MatchExtent.R
@@ -2,45 +2,40 @@
 #'
 #'@param x Data to crop and mask. May be a \code{raster} object or a path to a raster file.
 #'@param ref Reference raster. May be a \code{raster} object or a path to a raster file.
-#'@param output_dir Path to the output directory. If not provided, output is saved in the same directory as \code{x}.
-#'@param output_name Filename of the resampled raster without extension. If not provided \code{x} name is used with an appendix (_resampled)
-#'@param output_format Extension of the resampled raster. (Default=tif).
-#'@param overwrite Logical. Should the output raster be overwritten?
-
-
-
+#'@param filename Character. Output filename without directory. Can contain file extension (supported by writeRaster)
+#'@param outdir Character. Output directory. If not provided will be written in the same directory as \code{x}. If \code{x}
+#'is in memory, will be written in working directory
+#'@param ... Other arguments passed to \code{writeRaster} (e.g. format, overwrite ...)
 
 MatchExtent <- function(x,
                         ref,
-                        output_dir=NA,
-                        output_name=NA,
-                        output_format="tif",
-                        overwrite=TRUE) {
+                        filename='',
+                        outdir=NA,
+                        ...) {
 
-  #check if input reference image is raster or rasterstack
-  if(!class(ref)[1]== "RasterLayer") ref <- raster(ref)
+  filename <- trimws(filename)
 
-  #if x is not a rasterlayer or stack the read it from disk as stack
-  if(!class(x)[1]== "RasterLayer" || !class(x)[1]== "RasterStack") x <- stack(x)
+  #inputs can be either paths to rasters or raster objects
+  #check if input reference image is raster
+  if(!(class(ref)[1]== "RasterLayer"|class(ref)[1]== "RasterStack"|class(ref)[1]== "RasterBrick")) ref <- raster::raster(ref)
+  if(!(class(x)[1]== "RasterLayer"|class(x)[1]== "RasterStack"|class(x)[1]== "RasterBrick")) x <- raster::stack(x)
 
-
-  out <- crop(x, ref) #crop
-  out <- mask(out, ref)       #mask
-
-  #write output
-  #if output name is not provided, then write the resampled image with modified filename
-  if(is.na(output_name)) {
-    output_name <- create_name(r=x, output_format = "tif",odix="_crop")
+  if(is.na(crs(x))){
+    warning("x doesn't have any specified CRS")
+  }else if(!compare(crs(x),crs(ref))){
+    warning("x and ref don't have the same CRS")
   }
 
-  #if output_dir is NA then write image in the same dir
-  if(is.na(output_dir)) {
-    if(class(r)[1]== "RasterLayer") output_dir <- dirname(x@file@name)
-    if(class(r)[1]== "RasterStack") output_dir <- dirname(x@layers[[1]]@file@name)
+  out <- raster::crop(x,ref)
+
+  if (filename==''){
+    out <- raster::mask(out,ref)
+  }else{
+    if(is.na(outdir) & !raster::filename(x)=="") outdir <- dirname(x@file@name)
+    if(is.na(outdir) & raster::filename(x)=="") outdir <- getwd()
+    filename <- file.path(outdir,filename)
+    out <- raster::mask(out,ref,filename=filename)
   }
 
-  #construct output_image based on output_dir and output_name
-  output_image <- paste0(output_dir,"/", output_name)
-
-  writeRaster(out,output_image, overwrite=overwrite)
+  return(out)
 }
