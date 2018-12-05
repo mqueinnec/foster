fdc7daaff8d01fac7dabe2402ff7c0f83871aefb
mqueinnec <martinqueinnec@gmail.com>
2018-12-05 12:10:28 -0800

    Update MatchResolution

--- R/MatchResolution.R
+++ R/MatchResolution.R
@@ -1,42 +1,46 @@
-#'Resample lidar-based rasters to match satellite images
+#'Resample a raster from a reference raster
 #'
 #'@param x Data to resample. May be an existing \code{raster} object or a path to a raster file.
 #'@param ref Reference raster (e.g. wall-to-wall satellite image). \code{x} will be resampled to match \code{ref}. May be an existing \code{raster} object or a path to a raster file.
-#'@param output_dir Path to the output directory. If not provided, output is saved in the same directory as \code{x}.
-#'@param output_name Filename of the resampled raster without extension. If not provided \code{x} name is used with an appendix (_resampled)
-#'@param output_format Extension of the resampled raster. (Default=tif).
-#'@param overwrite Logical. Should the output raster be overwritten? (Default=TRUE)
+#'@param method Character. Method used to compute values for the resampled raster. Can be \code{'bilinear'} for bilinear
+#'interpolation or \code{'ngb'} for nearest neighbor interpolation.
+#'@param filename Character. Output filename without directory. Can contain file extension (supported by writeRaster)
+#'@param outdir Character. Output directory. If not provided will be written in the same directory as \code{x}. If \code{x}
+#'is in memory, will be written in working directory
+#'@param ... Other arguments passed to \code{writeRaster} (e.g. format, overwrite ...)
 
+MatchResolution <- function(x,
+                               ref,
+                               method='bilinear',
+                               filename='',
+                               outdir=NA,
+                               ...){
 
-
-MatchResolution <- function(x, #image to resample
-                     ref, # reference
-                     output_dir=NA,
-                     output_name=NA,
-                     output_format="tif",
-                     overwrite=TRUE) {
-
-  # ?assertive::is_class()
+  filename <- trimws(filename)
 
   #inputs can be either paths to rasters or raster objects
   #check if input reference image is raster
-  if(!class(ref)[1]== "RasterLayer") ref <- raster(ref)
-  if(!class(x)[1]== "RasterLayer") x <- raster(x)
+  if(!(class(ref)[1]== "RasterLayer"|class(ref)[1]== "RasterStack"|class(ref)[1]== "RasterBrick")) ref <- raster::raster(ref)
+  if(!(class(x)[1]== "RasterLayer"|class(x)[1]== "RasterStack"|class(x)[1]== "RasterBrick")) x <- raster::raster(x)
 
-  #if output name is not provided, then write the resampled image with modified filename
-  if(is.na(output_name)) {
-    output_name <- create_name(r=x, output_format = "tif")
+  #Check if x and ref have the same coordinate system
+  if(!raster::compareCRS(crs(x),crs(ref))){
+    warning("x and ref don't have the same CRS. x is projected to ref CRS before resampling")
+    x <- raster::projectRaster(x,crs(ref))
   }
 
-  #if output_dir is NA then write image in the same dir
-  if(is.na(output_dir))  output_dir <- dirname(x@file@name)
-
-  #construct output_image based on output_dir and output_name
-  output_image <- paste0(output_dir,"/", output_name)
-
-  r <- raster::resample(x = x,
-                        y = ref,
-                        filename=output_image,
-                        method="bilinear",
-                        overwrite=overwrite)
+  #If a filename is provided, write the output to file on disk
+  #If it is not, it tries to process it in memory.
+  #If the output is too big to be processed in memory, it'll be written to file anyway (in rasterTmpFile())
+  if(filename==''){
+    #We don't give filename argument, it will try to process in memory
+    out <- raster::resample(x=x,y=ref,method=method,...)
+  }else{
+    #Check ourdir argument. If NA, set to the basename of x. If x is not from a file, outDir set to getwd()
+    if(is.na(outdir) & !raster::filename(x)=="") outdir <- dirname(x@file@name)
+    if(is.na(outdir) & raster::filename(x)=="") outdir <- getwd()
+    filename <- file.path(outdir,filename)
+    out <- raster::resample(x=x,y=ref,method=method,filename=filename,...)
+  }
+  return(out)
 }
