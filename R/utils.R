a9f02590b76414d4014f6c439912a9b751617ab3
mqueinnec <martinqueinnec@gmail.com>
2019-01-22 14:07:10 -0800

    Add defautl summary + unwrap indices to utils

--- R/utils.R
+++ R/utils.R
@@ -1,4 +1,20 @@
-
+#'Calculate median, IQR and Theil Sen slope
+#'
+#'This function is usually called within \code{\link[foster]{temporalMetrics}}
+#'
+#'@param x Vector of numeric values
+#'@return A list with median, IQR and slope
+#'
+#'
+defaultTemporalSummary <- function(x) {
+  list(
+    mean=mean(x,na.rm=T),
+    sd=sd(x,na.rm=T),
+    median=median(x, na.rm = T),
+    IQR = IQR(x, na.rm = T),
+    slope = as.numeric(wql::mannKen(x)[1])
+  )
+}
 
 remove_extension <- function(x){ #copied from tools::file_path_sans_ext
   sub("([^.]+)\\.[[:alnum:]]+$", "\\1", x)
@@ -10,6 +26,27 @@ create_name <- function(r,output_format="tif",odix="_resampled") {
   paste0(input_basename, odix, ".",output_format)
 
 }
+
+unwrap_indices <- function(x,y){
+  #x is a list
+  #y is a stacked index dataset, at one timestep
+
+  for (n in 1:length(names(y))){
+    if(class(y)[1]=="SpatialPointsDataFrame"){
+      x[[names(y)[n]]] <- c(x[[names(y)[n]]],y[names(y)[n]])
+    }else{
+      x[[names(y)[n]]] <- c(x[[names(y)[n]]],raster::subset(y,names(y)[n]))
+    }
+  }
+
+  if(class(y)[1]=="SpatialPointsDataFrame"){
+    x <- lapply(x, function(x) do.call(cbind,x))
+  }else{
+    x <- lapply(x,function(x) raster::stack(x))
+  }
+  return(x)
+}
+
 
 
 
