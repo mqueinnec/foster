c0ca49aea47baec1ccaccb075a6ecab1c4017c98
mqueinnec <martinqueinnec@gmail.com>
2019-01-22 14:04:43 -0800

    Update data prep

--- R/MatchExtent.R
+++ R/MatchExtent.R
@@ -1,34 +1,62 @@
+#'Match the extent of a reference raster
+#'
 #'Crop and mask input raster to match the extent of the reference
 #'
-#'@param x Data to crop and mask. May be a \code{raster} object or a path to a raster file.
+#'\code{x} and \code{ref} need to have the same CRS, spatial resolution and origin. If this is not the case, you can use \code{\link[foster]{matchResolution}}. For more information about cropping and masking function you can refer to \code{\link[raster]{crop}} and \code{\link[raster]{mask}}
+#'
+#'@param x Data to crop and mask. May be a \code{Raster*} object or a path to a raster file.
 #'@param ref Reference raster. May be a \code{raster} object or a path to a raster file.
 #'@param mask logical. Should x be masked by \code{ref} cells that have the value \code{maskValue}
 #'@param maskValue logical. Value of \code{ref} cells that should be masked in \code{x}
 #'@param filename Character. Output filename without directory. Can contain file extension (supported by writeRaster)
 #'is in memory, will be written in working directory
 #'@param ... Other arguments passed to \code{writeRaster} (e.g. format, overwrite ...)
+#'@return A \code{Raster*} object
 
 matchExtent <- function(x,
                         ref,
-                        mask=T,
+                        mask=F,
+                        inverse=FALSE,
                         maskValue=NA,
+                        updatevalue=NA,
+                        updateNA = FALSE,
                         filename='',
                         ...) {
 
-  filename <- trimws(filename)
+  if(!any(class(x)[1] %in% c('RasterLayer','RasterStack','RasterBrick'))) {
+    stop("x must be a Raster object")
+  }
+
+  if(!any(class(ref)[1] %in% c('RasterLayer','RasterStack','RasterBrick'))) {
+    stop("ref must be a Raster object")
+  }
 
   if(!raster::compareCRS(x,ref) | !all(raster::origin(x)==raster::origin(ref)) | !all(raster::res(x)==raster::res(ref))) {
     stop("x and ref don't have the same CRS, origin or spatial resolution. Consider resampling before using MatchExtent")
   }
 
-  if(mask){
-    x.crop <- raster::crop(x,ref,filename='')
-  }else{
-    x.crop <- x
-  }
+  extent.x <- extent(x)
+  extent.ref <- extent(ref)
+  area.x <- (extent.x@xmax - extent.x@xmin)*(extent.x@ymax-extent.x@ymin)
+  area.ref <- (extent.ref@xmax - extent.ref@xmin)*(extent.ref@ymax-extent.ref@ymin)
 
-  out <- raster::mask(x.crop,ref,maskValue=maskValue,filename=filename)
-  names(out) <- names(x)
+  if(area.x<area.ref){
+    x.extent <- raster::extend(x,ref[[1]],filename='')
+  }else if(extent(x)>extent(ref)){
+    x.extent <- raster::crop(x,ref[[1]],filename='')
+  }else{
+    x.extent <- x
+  }
+  names(x.extent) <- names(x)
+
+  if(mask){
+    out <- raster::mask(x=x.extent,mask=ref[[1]],inverse=inverse,maskValue=maskValue,updatevalue=updatevalue,updateNA=updateNA,filename=filename,...)
+  }else{
+    out <- x.extent
+    if (filename != ""){
+      out <- writeRaster(out,filename=filename,...)
+    }
+  }
 
   return(out)
 }
--- R/MatchResolution.R
+++ R/MatchResolution.R
@@ -1,4 +1,8 @@
-#'Resample a raster from a reference raster
+#'Resample a raster from a reference
+#'
+#'Resample a raster from a reference
+#'
+#'\code{x} and \code{ref} must have defined CRS. You can assign a CRS using \code{\link[raster]{crs}}. If the CRS don't match, \code{x} is projected to \code{ref} CRS. \code{x} keeps its original extent, even if \code{ref} extent is larger.
 #'
 #'@param x \code{Raster*} object to resample
 #'@param ref Reference \code{Raster*} object with parameters that \code{x} should be resampled to.
@@ -6,7 +10,8 @@
 #'interpolation or \code{'ngb'} for nearest neighbor interpolation.
 #'@param filename Character. Output filename without directory. Can contain file extension (supported by writeRaster)
 #'is in memory, will be written in working directory
-#'@param ... Other arguments passed to \code{writeRaster} (e.g. format, overwrite ...)
+#'@param ... Other arguments passed to \code{\link[raster]{writeRaster}}
+#'@return A \code{Raster*} object
 
 matchResolution <- function(x,
                                ref,
@@ -14,7 +19,6 @@ matchResolution <- function(x,
                                filename='',
                                ...){
 
-  filename <- trimws(filename)
 
   if(!class(x)[1] %in% c('RasterLayer','RasterBrick','RasterStack')){
     stop("x must be a Raster object")
@@ -32,8 +36,13 @@ matchResolution <- function(x,
     x <- raster::projectRaster(x,crs(ref))
   }
 
-  #We crop ref to x extent. It avoids creating a large resampled x if ref extent is much larger than xs
-  ref_crop <- raster::crop(ref,x,filename='')
+  if(raster::extent(ref) > raster::extent(x)){
+    #We crop ref to x extent. It avoids creating a large resampled x if ref extent is much larger than xs
+    ref_crop <- raster::crop(ref,x,filename='')
+  }else{
+    ref_crop <- ref
+  }
+
 
   #Resampling
   out <- raster::resample(x=x,y=ref_crop,method=method,filename=filename,...)
