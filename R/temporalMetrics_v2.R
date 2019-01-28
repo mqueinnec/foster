8700049c311ef6cda6dbd61f127027c9cf10e54b
mqueinnec <martinqueinnec@gmail.com>
2019-01-28 15:20:21 -0800

    temporalMetrics_v2 supports parallel processing

--- R/temporalMetrics_v2.R
+++ R/temporalMetrics_v2.R
@@ -1,17 +1,3 @@
-default_temporal_summary <- function(x) {
-  list(
-    median=median(x, na.rm = T),
-    IQR = IQR(x, na.rm = T),
-    slope = as.numeric(wql::mannKen(x)[1])
-  )
-}
-
-myfunction <- function(x) {
-  list(
-    median = median(x)
-  )
-}
-
 #'Calculate temporal metrics
 #'
 #'@param x Input \code{raster::stack} object containing a time series of spectral indices (e.g. NDVI). Input stack may be generated with \code{calcIndices}
@@ -21,62 +7,48 @@ myfunction <- function(x) {
 #'@param output_format Extension of the resampled raster. (Default=tif).
 #'@param overwrite Logical. Should the output raster be overwritten?
 
-TemporalMetrics_v2 <- function(s,
-                             fun="median",
-                             filename='',
-                             ...) {
-  #input is a raster stack or???
-  #user should be able to specify the function
+temporalMetrics_v2 <- function(s,
+                            metrics="defaultTemporalSummary",
+                            prefix=NULL,
+                            filename='',
+                            no_cores=1,
+                            par=F,
+                            ...) {
 
-  #Checking names of the input time series of indices
-  prefix <- names(s)[1]
+  if(is.null(prefix)) prefix <- names(s)[1]
 
-  textFun <- character()
-  for (k in 1:length(fun)) {
-    if (fun[k]=='slope') {
-      fun[k] <- paste0(prefix,"_slope=as.numeric(wql::mannKen(value)[1])")
-    }else{
-      fun[k] <- paste0(prefix,"_",fun[k],"=",fun[k],"(value)")
-    }
-    textFun <- paste(fun,collapse=",")
+  if(!is.character(metrics)) stop("metrics must be a character (function name)")
+  if(any(grepl("\\(|\\)",metrics))) stop("metrics must be a character without brackets ()")
+
+  eval.fun = character()
+  for(f in 1:length(metrics)){
+    eval.fun[f] <- paste0(metrics[f],"(value)")
   }
 
-  #currently supporting stack only
+  eval.fun <- paste(eval.fun,collapse=",")
+
   if (class(s)[1]=="SpatialPointsDataFrame") {
     ind.df <- raster::as.data.frame(s) #xy arg is not necessary for spdf, it adds a column
+    ind.dt <- data.table::as.data.table(ind.df)
+    #convert to long
+    ind.dt.long <-  data.table::melt(ind.dt, id.vars = c("x","y"),value.name='value')
+    #generate summary
+    result <- ind.dt.long[,j=eval(parse(text=eval.fun)),by=list(x,y)]
+    # output should be spatial object
+    coordinates(result) <- ~x+y
+    names(result) <- paste0(prefix,"_",names(result))
+
+    raster::crs(result) <- raster::crs(s)
   }else{
-    ind.df <- raster::as.data.frame(s, xy=T)
-  }
-  ind.dt <- data.table::as.data.table(ind.df)
+    if(par){
+      raster::beginCluster(no_cores)
+      result <- raster::clusterR(s,fun=calc,args=list(fun=eval(parse(text=metrics))),filename=filename)
+      raster::endCluster()
+    }else{
+      result <- raster::calc(s,fun=eval(parse(text=metrics)),filename=filename,...)
+    }
 
-  #convert to long
-  ind.dt.long <-  melt(ind.dt, id.vars = c("x","y"))
-
-  #filter NAs
-  ind.dt.long <- ind.dt.long[!is.na(ind.dt.long$value),]
-
-  #generate summary
-  #result <- ind.dt.long[,j=eval(parse(text=paste0(fun,"(value)"))),by=list(x,y)]
-  result <- eval(parse(text=paste0("ind.dt.long[,j=list(",textFun,"),by=list(x,y)]")))
-  # result <- ind.dt.long[, j=myFun(value),by = list(x,y)]
-
-  # output should be spatial object
-  coordinates(result) <- ~x+y
-  #Check the class of the input and if a filename was provided to determine
-  #if the output should be a raster or a spatial point
-  if (!((class(s)[1] == "SpatialPointsDataFrame")&(filename==""))){
-    gridded(result) <- TRUE
-    r <- stack(result)
-  }else{
-    r <- result
   }
 
-  #Setting back the CRS from the input (lost when computing metrics with fun)
-  crs(r) <- crs(s)
-
-  if(filename != "") {
-    out <- raster::writeRaster(out,filename=filename,...) ##bylayer=T, suffix=names(r)
-  }
-
-  return(r)
+  return(result)
 }
