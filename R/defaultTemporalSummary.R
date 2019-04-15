4b0825f38ae0f2ab403daffbd4975407d2d1bf8c
mqueinnec <martinqueinnec@gmail.com>
2019-04-15 15:13:24 -0700

    defaultTemporalSummary checks if wql is installed for mannKenn slope

--- R/defaultTemporalSummary.R
+++ R/defaultTemporalSummary.R
@@ -6,6 +6,9 @@
 #'@return A list with median, IQR and slope
 #'@export
 defaultTemporalSummary <- function(x) {
+  if (!requireNamespace("wql",quietly=TRUE)){
+    stop("Package \"wql\" needed by defaultTemporalSummary to calculate Then-Sen slope. Please install it.")
+  }
   c(
     mean=mean(x,na.rm=T),
     sd=sd(x,na.rm=T),
