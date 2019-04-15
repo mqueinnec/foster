6e5bbbd389da8f827d12e109ca4614673bb50b83
mqueinnec <martinqueinnec@gmail.com>
2019-04-15 15:16:58 -0700

    predictTrgs supports parallel processing and processes data row by row (default)

--- R/predictTrgs.R
+++ R/predictTrgs.R
@@ -1,7 +1,14 @@
 #'Returns Y values at targets
 #'
+#'Finds the k-NN of targets and impute response variables
+#'
+#'By default, the data is processed row by row to avoid creating very large objects (several Gb) that couldn't be stored in memory. However, processing data row by row slows down processing. Depending on the amount of RAM available on your computer and on the size of the area where k-NN need to be calculated, it is possible to process multiple rows at the same time and considerably reduce processign times.
+#'
+#'
+#'
 #'@param model A model trained at reference locations, ususally created from \code{modelFoster}
-#'@param Xtrgs Predictors (X) values at targets
+#'@param x Predictors (X) values at targets
+#'@param nrows number of rows processed at a time. Default is 1.
 #'@param filename Character. Path to file written to disk (may include the extension)
 #'@param par Logical. Should imputation be performed on parallel threads?
 #'@param n.cores Integer. Number of parallel threads (relevant only if par=TRUE)
@@ -9,72 +16,145 @@
 #'@export
 
 predictTrgs <- function(model=NULL,
-                         X.trgs=NULL,
-                         filename='',
-                         par=F,
-                         n.cores=1,
-                         ...){
+                        x=NULL,
+                        filename='',
+                        no_cores=1,
+                        nrows = 1,
+                        ...){
+
+  #Check args
+  if (no_cores <= 0) stop("no_cores must be > 0")
+  if (is.null(model)) stop("Need to provide trained model")
+  if(is.null(x)) stop("Need to provide targets X values")
+  if(!all(colnames(model$xRefs) %in% names(x))) stop("Predictor variables names don't match")
 
   #Define predict function
   predFun.yai <- function(model,data){
-    rownames(data)<-paste0("trgs_",rownames(data)) ## so that newtargets doesn't get confused by duplicate row IDs.
     newtrgs <- yaImpute::newtargets(model,data)
     trgs.imputed <- yaImpute::impute(newtrgs,observed=F,vars=colnames(newtrgs$yRefs))
-    return(as.matrix(cbind(trgs.imputed,neiID=as.numeric(as.character(newtrgs$neiIdsTrgs))),rownames.force=F))
+    preds <- as.matrix(cbind(trgs.imputed,neiID=as.numeric(as.character(newtrgs$neiIdsTrgs))),rownames.force=F)
+    return(preds)
   }
 
-
-  ###########################
-
-  if (is.null(model)) stop("Need to provide trained model")
-  if(is.null(X.trgs)) stop("Need to provide targets X values")
-
-  # if(class(X.trgs)[1] %in% c("RasterLayer","RasterStack","RasterBrick")){
-  #   coords.trgs <- data.frame(sp::coordinates(X.trgs))
-  #   Xtrgs <- raster::as.data.frame(X.trgs)
-  # }
-
-  #rownames(Xtrgs) <- paste0("trgs_",rownames(Xtrgs))
-
-  #Xtrgs.noNA <- complete.cases(Xtrgs)
-  #out <- data.frame(coords.trgs)
-
-  ##########################
-  #Set default values
-  if(!all(colnames(model$xRefs) %in% names(X.trgs))) stop("Predictor variables names don't match")
-
-  if(par){
-    raster::beginCluster(n=n.cores)
-    raster::clusterR(X.trgs,predict,args=list(model=model,fun=predFun.yai,na.rm=T,index=1:(length(colnames(model$yRefs))+1)),filename=filename,progress='text',m=2)
-    raster::endCluster()
-  }else{
-    raster::predict(object=X.trgs,model=model,fun=predFun.yai,na.rm=T,index=1:(length(colnames(model$yRefs))+1),filename=filename,progress='text')
+  #If empty filename write to temp
+  if (filename=="") {
+    filename <- rasterTmpFile()
   }
-  #Xtrgs <- Xtrgs[Xtrgs.noNA,]
+
+  #Create empty raster where predictions will be saved
+  predrast <- brick(x,values=FALSE,nl=(length(colnames(model$yRefs))+1))
+
+  #Divide x in blocks. Make it changeable
+  tr <- raster::blockSize(x,minblocks = round(nrow(x)/nrows))
+
+  #Create progress bar
+  pb <- pbCreate(tr$n,label="predict",progress='text')
+
+  if (no_cores == 1) {
+
+    predrast <- writeStart(predrast, filename=filename, ...)
+
+    for (i in 1:tr$n) {
+
+      napred <- matrix(rep(NA, ncol(predrast) * tr$nrows[i] * nlayers(predrast)), ncol=nlayers(predrast))
+
+      blockVals <- data.frame(getValues(x,row=tr$row[i],nrows=tr$nrows[i]))
+
+      #Remove NAs from blockVals
+      blockVals <- stats::na.omit(blockVals)
+
+      if (nrow(blockVals)==0){
+        predv <- napred
+      } else {
+        #Change rownames to avoid false duplicates with yai
+        rownames(blockVals)<-paste0("trgs_",rownames(blockVals))
+        #Predict
+        predv <- predFun.yai(model,blockVals)
+      }
+      #Assign back NA
+      naind <- as.vector(attr(blockVals,"na.action"))
+      if(!is.null(naind)){
+        p <- napred
+        p[-naind,] <- predv
+        predv <- p
+        rm(p)
+      }
+      #Write preds to file
+      predrast <- writeValues(predrast, predv, tr$row[i])
+
+      raster::pbStep(pb,i)
+    }
 
 
-  # if(class(model) == 'yai') {
-  #   newtargets.model <- yaImpute::newtargets(model,newdata=Xtrgs)
-  #   Ytrgs <- yaImpute::impute(newtargets.model,observed=F,vars=colnames(newtargets.model$yRefs))
-  # }
+  }else{ #Parallel procesing
+    .sendCall <- eval( parse( text="parallel:::sendCall") )
 
-  #Ytrgs <- Ytrgs[,colnames(newtargets.model$yRefs)]
+    #Start clusters
+    beginCluster(no_cores)
 
-  # #Create a raster stack filled with Ytrgs values
-  # if(class(X.trgs)[1] %in% c("RasterLayer","RasterStack","RasterBrick")){
-  #   for(i in 1:length(Ytrgs)){
-  #     out <- eval(parse(text=paste0("data.frame(out,",colnames(Ytrgs)[i],"=NA)")))
-  #     out[Xtrgs.noNA,colnames(Ytrgs)[i]] <- Ytrgs[,colnames(Ytrgs)[i]]
-  #   }
-  #   coordinates(out) <- ~x+y
-  #   gridded(out) <- TRUE
-  #
-  #   out <- raster::stack(out)
-  #   crs(out) <- crs(X.trgs)
-  #
-  #   return(out)
-  # }else{
-  #   return(Ytrgs)
-  # }
+    cl <- getCluster()
+    on.exit(endCluster())
+    nodes <- length(cl)
 
+    #Define function that will be exectuted by clusters
+    clusfun <- function(fun,i) {
+      napred <- matrix(rep(NA,ncol(predrast)*tr$nrows[i]*nlayers(predrast)),ncol=nlayers(predrast))
+      blockVals <- data.frame(getValues(x,row=tr$row[i],nrows=tr$nrows[i]))
+      #Remove NAs from blockVals
+      blockVals <- stats::na.omit(blockVals)
+      if (nrow(blockVals)==0){
+        predv <- napred
+      } else {
+        #Change rownames to avoid false duplicates with yai
+        rownames(blockVals)<-paste0("trgs_",rownames(blockVals))
+        #Predict
+        predv <- fun(model,blockVals)
+      }
+      #Assign back NA
+      naind <- as.vector(attr(blockVals,"na.action"))
+      if(!is.null(naind)){
+        p <- napred
+        p[-naind,] <- predv
+        predv <- p
+        rm(p)
+      }
+      return(predv)
+    }
+
+    if(tr$n < nodes) { #If there are more nodes than blocks we don't need all clusters
+      nodes <- tr$n
+    }
+
+    #Export variables that are required by clusfun
+    #parallel::clusterExport(cl, c("tr","predrast","model","x"))
+
+    #Send function to be exectuted in clusters
+    for (i in 1:nodes){
+      .sendCall(cl[[i]],clusfun,list(fun=predFun.yai,i=i),tag=i)
+    }
+
+    for (i in 1:tr$n) {
+      pbStep(pb,i)
+
+      d <- snow::recvOneData(cl)
+
+      if (! d$value$success) stop("cluster error")
+
+      if (i==1) {
+        predrast <- writeStart(predrast,filename=filename,...)
+      }
+
+      predrast <- writeValues(predrast, d$value$value, tr$row[d$value$tag])
+      ni <- nodes + i
+
+      if (ni <= tr$n) {
+        .sendCall(cl[[d$node]],clusfun,list(fun=predFun.yai,ni),tag=ni)
+      }
+    }
+  }
+  try(names(predrast) <- c(colnames(model$yRefs),"neiID"),silent=T)
+  predrast <- writeStop(predrast)
+  pbClose(pb)
+  endCluster()
+  return(predrast)
 }
